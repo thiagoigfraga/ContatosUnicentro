aea2132f1d0a0ddf59f03719df06e4133ccedf9e
Thiago Garcia <thiagogarciafraga99@gmail.com>
2022-08-29 22:32:15 -0300

    Aula 1
    contatos em home.page.ts
    ion-list em home.page.html

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  contatos: string[];
 
-  constructor() {}
-
+  constructor() {
+    this.contatos = ['Carlos Eduardo', 'Regiane Orlovski', 'Kelly Wiggers'];
+  }
 }
